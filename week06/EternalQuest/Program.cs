744876ecd7e0d8352144dde51a9130fd3f6a81a2
luzbelx <thurealeidhan75@gmail.com>
2026-08-10 15:37:25 -0400

    Add documentation and enhance Main method
    Added comments to describe the program's functionality and enhancements.

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,22 @@
-using System;
+/*
+ * Eternal Quest Program
+ *
+ * Extra Creativity:
+ * In addition to the required goal functionality, this program includes
+ * a player level and rank system based on the user's score.
+ *
+ * The user can also view statistics showing completed and remaining goals.
+ *
+ * This extra functionality was added to make the program feel more like
+ * a real quest/progression system while keeping the original requirements.
+ */
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager goalManager = new GoalManager();
+
+        goalManager.Run();
     }
 }
